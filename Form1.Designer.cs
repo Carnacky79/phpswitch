fec156790ed13c660ca8cf8ca120343440bed4ee
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2019-12-19 13:16:50 +0100

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace PHP_Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Switch Versioin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select PHP Version";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(105, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "PHP 7.2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(34, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PHP 5.6";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PHP_Switch.Properties.Resources.accept_circle_100;
+            this.pictureBox1.Location = new System.Drawing.Point(66, 157);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(79, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(79, 291);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(235, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "Options";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 351);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PHP Switch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PHP_Switch
+{
+    public partial class Form1 : Form
+    {
+
+        public string PHP_5_Module = "LoadModule php5_module \"c:/php5/php5apache2_4.dll\"";
+        public string PHP_7_Module = "LoadModule php7_module \"c:/php/php7apache2_4.dll\"";
+
+        public string PHP_5_Dir = "PHPIniDir \"C:/php5\"";
+        public string PHP_7_Dir = "PHPIniDir \"C:/php\"";
+
+        public string httpConfPath = "C:/Apache24/conf/httpd.conf";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeTimer();
+
+            watchPHPVersion();
+
+            radioButton1.CheckedChanged += new EventHandler(RadioButtonCheckedChanged);
+            radioButton1.CheckedChanged += new EventHandler(RadioButtonCheckedChanged);
+
+            button1.Enabled = false;
+        }
+
+        private void RadioButtonCheckedChanged(object Sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changePHPVersion()
+        {
+            string text = File.ReadAllText(httpConfPath);
+            ServiceController sc = new ServiceController("ApacheServer");
+
+            if (radioButton1.Checked == true)
+            {
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    sc.Stop();
+                    text = text.Replace(PHP_7_Module, PHP_5_Module);
+                    text = text.Replace(PHP_7_Dir, PHP_5_Dir);
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(25));
+                    sc.Start();
+                }
+                else if(sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    text = text.Replace(PHP_7_Module, PHP_5_Module);
+                    text = text.Replace(PHP_7_Dir, PHP_5_Dir);
+                    sc.Start();
+                }
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    sc.Stop();
+                    text = text.Replace(PHP_5_Module, PHP_7_Module);
+                    text = text.Replace(PHP_5_Dir, PHP_7_Dir);
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(25));
+                    sc.Start();
+                }
+                else if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    text = text.Replace(PHP_5_Module, PHP_7_Module);
+                    text = text.Replace(PHP_5_Dir, PHP_7_Dir);
+                    sc.Start();
+                }
+            }
+
+            File.WriteAllText(httpConfPath, text);
+
+            button1.Enabled = false;
+        }
+
+        private void watchPHPVersion()
+        {
+            string line;
+
+            StreamReader file = new StreamReader(httpConfPath);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains(PHP_5_Module))
+                {
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    break;
+                } 
+                else if (line.Contains(PHP_7_Module))
+                {
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    break;
+                }
+            }
+
+            file.Close();
+        }
+
+
+        private void InitializeTimer()
+        {          
+            timer1.Interval = 500;
+            timer1.Tick += new EventHandler(Timer1_Tick);
+
+            // Enable timer.  
+            timer1.Enabled = true;
+        }
+
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+            ServiceController sc = new ServiceController("ApacheServer");
+
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                label1.Text = "Apache is running.";
+                pictureBox1.Image = PHP_Switch.Properties.Resources.accept_circle_100;
+                button2.Enabled = false;
+                button3.Enabled = true;
+            }
+
+            else if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                label1.Text = "Apache is stopped.";
+                pictureBox1.Image = PHP_Switch.Properties.Resources.remove_circle_100;
+                button2.Enabled = true;
+                button3.Enabled = false;
+            }
+            else
+            {
+                label1.Text = "Status pending...";
+                pictureBox1.Image = PHP_Switch.Properties.Resources.more_circle_100;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changePHPVersion();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceController sc = new ServiceController("ApacheServer");
+            sc.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ServiceController sc = new ServiceController("ApacheServer");
+            sc.Stop();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Form form2 = new options();
+            form2.Show();
+        }
+    }
+}
